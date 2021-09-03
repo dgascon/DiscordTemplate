ac20f5031f439fffd8d32ae068c66c248a12fb0e
dgascon <dgascon@student.42lyon.fr>
2021-09-03 19:28:38 +0200

    add exports

---
+++ index.js
@@ -0,0 +1,15 @@
+const Client = require('./Client.js');
+const ArrayDB = require('./ArrayDB.js');
+const Command = require('./Command.js');
+const Event = require('./Event.js');
+const JsonUtil = require('./JsonUtil.js');
+const Util = require('./Util.js');
+
+module.exports = {
+	Client: Client,
+	ArrayDB: ArrayDB,
+	Command: Command,
+	Event: Event,
+	JsonUtil: JsonUtil,
+	Util: Util
+}
